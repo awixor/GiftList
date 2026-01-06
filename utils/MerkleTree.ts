c23723f6429cead162c37c6b1575e0c86a6ed3de
EL HOUCINE AOUASSAR <el.houcine.aouassar@esportal.com>
2026-01-06 13:51:27 +0100

    Simplify mercle tree class logic

--- utils/MerkleTree.ts
+++ utils/MerkleTree.ts
@@ -1,79 +1,58 @@
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { bytesToHex } from "ethereum-cryptography/utils";
 
-interface ProofNode {
-  data: string;
-  left: boolean;
-}
-
-class MerkleTree {
-  private leaves: Uint8Array[];
-  private concat: (left: Uint8Array, right: Uint8Array) => Uint8Array;
+export class MerkleTree {
+  private layers: Uint8Array[][];
 
   constructor(leaves: string[]) {
-    this.leaves = leaves.map(Buffer.from).map(keccak256);
-    this.concat = (left: Uint8Array, right: Uint8Array) =>
-      keccak256(Buffer.concat([left, right]));
+    // [[H(A), H(B), H(C), H(D)]] => [[Root], [H(A+B), H(C+D)], [H(A), H(B), H(C), H(D)]]
+    this.layers = [leaves.map((leaf) => keccak256(Buffer.from(leaf)))];
+
+    // Build the tree upwards until we reach the root
+    while (this.layers[0].length > 1) {
+      this.layers.unshift(this.getNextLayer(this.layers[0]));
+    }
+  }
+
+  private getNextLayer(nodes: Uint8Array[]): Uint8Array[] {
+    const layer: Uint8Array[] = [];
+
+    for (let i = 0; i < nodes.length; i += 2) {
+      const left = nodes[i];
+      const right = nodes[i + 1];
+
+      // If no right sibling exists, move the left node up; otherwise, hash the pair
+      if (right) layer.push(keccak256(Buffer.concat([left, right])));
+      else layer.push(left);
+    }
+
+    return layer;
   }
 
   getRoot(): string {
-    return bytesToHex(this._getRoot(this.leaves));
+    return bytesToHex(this.layers[0][0]);
   }
 
-  getProof(
-    index: number,
-    layer: Uint8Array[] = this.leaves,
-    proof: ProofNode[] = []
-  ): ProofNode[] {
-    if (layer.length === 1) {
-      return proof;
-    }
+  getProof(index: number): { data: string; left: boolean }[] {
+    let proof = [];
+    let layerIdx = this.layers.length - 1; // Start from leaves
 
-    const newLayer: Uint8Array[] = [];
+    while (layerIdx > 0) {
+      const layer = this.layers[layerIdx];
+      const isRightSibling = index % 2 !== 0;
+      const pairIndex = isRightSibling ? index - 1 : index + 1;
 
-    for (let i = 0; i < layer.length; i += 2) {
-      const left = layer[i];
-      const right = layer[i + 1];
-
-      if (!right) {
-        newLayer.push(left);
-      } else {
-        newLayer.push(this.concat(left, right));
-
-        if (i === index || i === index - 1) {
-          let isLeft = !(index % 2);
-          proof.push({
-            data: isLeft ? bytesToHex(right) : bytesToHex(left),
-            left: !isLeft,
-          });
-        }
+      // prevent no right sibling error
+      if (pairIndex < layer.length) {
+        proof.push({
+          data: bytesToHex(layer[pairIndex]),
+          left: isRightSibling,
+        });
       }
+
+      index = Math.floor(index / 2);
+      layerIdx--;
     }
-
-    return this.getProof(Math.floor(index / 2), newLayer, proof);
-  }
-
-  // private function
-  private _getRoot(leaves: Uint8Array[] = this.leaves): Uint8Array {
-    if (leaves.length === 1) {
-      return leaves[0];
-    }
-
-    const layer: Uint8Array[] = [];
-
-    for (let i = 0; i < leaves.length; i += 2) {
-      const left = leaves[i];
-      const right = leaves[i + 1];
-
-      if (right) {
-        layer.push(this.concat(left, right));
-      } else {
-        layer.push(left);
-      }
-    }
-
-    return this._getRoot(layer);
+    return proof;
   }
 }
-
-export default MerkleTree;
